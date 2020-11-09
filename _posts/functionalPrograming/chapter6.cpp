ac252b7c5859f423ecdc9b57a5d67d84dca8f32d
revol300 <rlawjddyd300@naver.com>
2020-11-09 18:10:56 +0900

    chatper 6 update

--- _posts/functionalPrograming/chapter6.cpp
+++ _posts/functionalPrograming/chapter6.cpp
@@ -179,3 +179,51 @@ class null_param {};
 template <class Sig, class F>
 class memoize_helper;
 
+template <class Result, class... Args, class F>
+class memoize_helper<Result(Args...), F> {
+  private:
+    using function_type = F;
+    using args_tuple_type = std::tuple<std::decay_t<Args>...>;
+
+    function_type f;
+    mutable std::map<args_tuple_type, Result> m_cache;
+    mutable std::recursive_mutex m_cache_mutex;
+
+  public:
+    template <typename Function>
+      memoize_helper(Function&&f, null_param) : f(f) {}
+      memoize_helper(const memoize_helper& other) : f(other.f) {}
+
+      template <class... InnerArgs>
+      Result operator()(InnerArgs&&... args) const {
+        std::unique_lock<std::recursive_mutex> lock{m_cache_mutex};
+
+        const auto args_tuple = std::make_tuple(args...);
+        const auto cached = m_cache.find(args_tuple);
+
+        if (cached != m_cache.end()) {
+          return cached->second;
+        } else {
+          auto&& result = f(
+              *this,
+              std::forward<InnerArgs>(args)...);
+          m_cache[args_tuple] = result;
+          return result;
+        }
+      }
+};
+
+template <class Sig, class F>
+memoize_helper<Sig, std::decay_t<F>> make_memoized_r(F&& f) {
+  return {std::forward<F>(f), detail::null_param()};
+}
+
+//fib의 메모화된 버전 create
+auto fibmemo = make_memoized_r<unsigned int(unsigned int)>(
+    [](auto& fib, unsigned int n) {
+      std::cout << "Calculating" << n << "!\n";
+      return n == 0 ? 0
+           : n == 1 ? 1
+           : fib(n-1)+fib(n-2);
+  }
+);
